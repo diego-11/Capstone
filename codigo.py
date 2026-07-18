41bbb54d4885588f04bf4c274df6a69e382de61b
diego-11 <diego.isiquita@correounivalle.edu.co>
2026-07-17 19:04:34 -0500

    Se crearon archivos base para desarrollo del capstone

---
+++ codigo.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("rabieelkharoua/predict-online-gaming-behavior-dataset")
+
+print("Path to dataset files:", path)
+print("Funcionaaa")
